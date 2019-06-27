abd180fb7d59e4b3617f2c44660485e816292e2d
dinosaurTHJ <707552155@qq.com>
2019-06-27 15:42:03 +0800

    thj 2019-6-25

--- src/test/java/T.java
+++ src/test/java/T.java
@@ -1,15 +1,8 @@
 import com.lovo.disaster.db.GetSession;
-import com.lovo.disaster.system.bean.SysRole;
-import com.lovo.disaster.system.bean.SysUser;
-import com.lovo.disaster.system.dao.IUserDao;
-import com.lovo.disaster.system.service.IUserService;
-import com.lovo.disaster.system.service.impl.UserServiceImpl;
+import com.lovo.disaster.pwd.bean.DiseaseBean;
+import com.lovo.disaster.pwd.dao.IDiseaseDao;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class T {
 
@@ -17,15 +10,9 @@ public class T {
     public void ttt(){
 
         SqlSession session = GetSession.creatSession();
+//      DiseaseBean b= session.getMapper(IDiseaseDao.class).findById(1);
 
         System.out.println(session);
-    }
-    @Test
-    public void t(){
-        SqlSession session = GetSession.creatSession();
-        IUserDao dao = session.getMapper(IUserDao.class);
-        dao.delUserById(8);
-        session.commit();
         session.close();
     }
 }
